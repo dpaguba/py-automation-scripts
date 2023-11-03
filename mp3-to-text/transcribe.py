0297a572a527f3c5d111aac5d33bf288457815c2
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-03 11:09:28 +0100

    feat: convert mp3 file to wav

---
+++ mp3-to-text/transcribe.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr
+from os import path
+from pydub import AudioSegment
+
+# convert mp3 file to wav
+sound = AudioSegment.from_mp3("transcript.mp3")
+sound.export("transcript.wav", format="wav")
+
+
