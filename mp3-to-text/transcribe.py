f8fa5e9a404b3ef967afd4ce80e5ff5ed75c4864
Dmytro Pahuba <dpaguba@gmail.com>
2023-11-04 22:20:47 +0100

    feat: recognize speech in the audio file

--- mp3-to-text/transcribe.py
+++ mp3-to-text/transcribe.py
@@ -4,3 +4,13 @@ from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
 r = sr.Recognizer()
+
+def transcribe_audio(path):
+    # use the audio file as the audio source
+    with sr.AudioFile(path) as source:
+        audio_listened = r.record(source)
+        # try converting it to text
+        text = r.recognize_google(audio_listened, language="en-EN")
+    return text
+
+
